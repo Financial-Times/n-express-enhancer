587b8ac5f87b841f5f4606565a0dbb6e71796fa0
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-05-01 14:35:34 +0100

    test cover update  🐿 v2.8.0

--- src/__tests__/add-meta.js
+++ src/__tests__/add-meta.js
@@ -1,15 +1,7 @@
 import addMeta from '../add-meta';
 
 describe('addMeta', () => {
-	it('create enhancer that add meta to action function', () => {
-		const actionFunction = (params, meta) => meta;
-		const addingMeta = { service: 'some-service' };
-		const enhanced = addMeta(addingMeta)(actionFunction);
-		const result = enhanced({}, {});
-		expect(result).toEqual(addingMeta);
-	});
-
-	it('create enhancer that can add meta to action function bundle', async () => {
+	it('create enhancer that can add meta to action functions both async/non-async', async () => {
 		const actionFunctionBundle = {
 			methodA: (params, meta) => meta,
 			methodB: async args => args,
@@ -22,6 +14,33 @@ describe('addMeta', () => {
 		expect(resultB).toEqual(addingMeta);
 	});
 
+	it('catches errors thrown from action function both async/non-async', async () => {
+		const errorInstance = { status: 404, meesage: 'some error message' };
+		const actionFunction = () => {
+			throw errorInstance;
+		};
+		const asyncActionFunction = async () => {
+			throw await errorInstance;
+		};
+		const addServiceMeta = addMeta({ service: 'some-service' });
+		try {
+			addServiceMeta(actionFunction)();
+		} catch (e) {
+			expect(e).toEqual(errorInstance);
+		}
+		try {
+			await addServiceMeta(asyncActionFunction)();
+		} catch (e) {
+			expect(e).toEqual(errorInstance);
+		}
+	});
+
+	it('throws error if input of addMeta is not an object, e.g. in case the meta is forgotten', () => {
+		const operationFunction = () => {};
+		expect(() => addMeta('')).toThrowErrorMatchingSnapshot();
+		expect(() => addMeta(operationFunction)).toThrowErrorMatchingSnapshot();
+	});
+
 	// it('throws error if created enhancer used on operation function', () => {
 	// 	const actionFunction = (params, meta) => meta;
 	// 	const operationFunction = meta => meta;
@@ -31,10 +50,4 @@ describe('addMeta', () => {
 	// 		operationFunction,
 	// 	});
 	// });
-
-	it('throws error if input of addMeta is not an object, e.g. in case the meta is forgotten', () => {
-		const operationFunction = () => {};
-		expect(() => addMeta('')).toThrowErrorMatchingSnapshot();
-		expect(() => addMeta(operationFunction)).toThrowErrorMatchingSnapshot();
-	});
 });
